f77a3c32b077d956730a0a14f54a53e93126d9fc
Chris Palmer <christopherpalmer283@gmail.com>
2023-07-26 16:33:53 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+import axios from "axios";
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [img, setImg] = useState("");
+
+  function handleSearch(event) {
+    setSearchQuery(event.target.value);
+  }
+
+  async function getImage() {
+    try {
+      const API = `http://localhost:8090/photos?subject=${searchQuery}`;
+      const res = await axios.get(API);
+      // console.log(res.data);
+      setImg(res.data[0].img_url);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Find any image</h1>
+      <input
+        type="text"
+        placeholder="Enter image subject"
+        onChange={handleSearch}
+      />
+      <button onClick={getImage}>Search</button>
+      {img && <img src={img} alt={searchQuery} />}
     </div>
   );
 }
